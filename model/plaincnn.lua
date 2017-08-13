25cdfa910a6f3431dcd75b6f3c7d8d650cdcedc8
Unknown <jaytin1218@hotmail.com>
2017-08-13 21:34:55 +0800

    Mod: Change sigmoid to leakyReLU

--- model/plaincnn.lua
+++ model/plaincnn.lua
@@ -30,14 +30,17 @@ function plaincnn(options)
     -- MODEL Build convolution
     local modelConvolution = nn.Sequential()
     --modelConvolution:add(nn.TemporalConvolution(300, 1, kernalWidth)):add(nn.Sigmoid())
-    modelConvolution:add(nn.TemporalConvolution(300, channelSize, kernalWidth)):add(nn.Sigmoid())
+    --modelConvolution:add(nn.TemporalConvolution(300, channelSize, kernalWidth)):add(nn.Sigmoid())
+    modelConvolution:add(nn.TemporalConvolution(300, channelSize, kernalWidth)):add(nn.LeakyReLU())
     for i = 1, convLayer - 3 do
         local localPad = nn.Sequential():add(nn.Padding(2, -leftPadSize)):add(nn.Padding(2, rightPadSize))
-        modelConvolution:add(localPad):add(nn.TemporalConvolution(channelSize, channelSize, kernalWidth)):add(nn.Sigmoid())
+        modelConvolution:add(localPad):add(nn.TemporalConvolution(channelSize, channelSize, kernalWidth)):add(nn.LeakyReLU())
+        --modelConvolution:add(localPad):add(nn.TemporalConvolution(channelSize, channelSize, kernalWidth)):add(nn.Sigmoid())
     end
 
     local finalPad = nn.Sequential():add(nn.Padding(2, -leftPadSize)):add(nn.Padding(2, rightPadSize))
-    modelConvolution:add(finalPad):add(nn.TemporalConvolution(channelSize, math.floor(channelSize / 2), kernalWidth)):add(nn.Sigmoid())
+    modelConvolution:add(finalPad):add(nn.TemporalConvolution(channelSize, math.floor(channelSize / 2), kernalWidth)):add(nn.LeakyReLU())
+    --modelConvolution:add(finalPad):add(nn.TemporalConvolution(channelSize, math.floor(channelSize / 2), kernalWidth)):add(nn.Sigmoid())
     finalPad = nn.Sequential():add(nn.Padding(2, -leftPadSize)):add(nn.Padding(2, rightPadSize))
     modelConvolution:add(finalPad):add(nn.TemporalConvolution(math.floor(channelSize / 2), 1, kernalWidth)):add(nn.Sigmoid())
 
